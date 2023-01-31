0b3666a7c556c589fac5faec3da13f92f17d54ec
MikhailChrn <mikhail.cherneyev@yandex.ru>
2023-01-31 13:42:41 +0400

    Zero commit 1.Create MySQL Database; 2.Add dependencies to pom.xml file; 3.Setting up web.xml file; 4.Setting up applicationContext.xml file; 5.Create Employee java class; 6.Create EmployeeDAO class; 7.Create Controller class; 8.Create first JSP view;

---
+++ src/main/java/org/company/spring/mvc_hibernate_firstapp/dao/EmployeeDAO.java
@@ -0,0 +1,9 @@
+package org.company.spring.mvc_hibernate_firstapp.dao;
+
+import org.company.spring.mvc_hibernate_firstapp.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeDAO {
+    public List<Employee> getAllEmployees();
+}
